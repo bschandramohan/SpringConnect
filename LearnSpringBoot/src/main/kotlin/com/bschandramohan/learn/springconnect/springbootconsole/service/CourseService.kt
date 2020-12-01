9c8dee27ef47eb0fb01b713f7ff11949322cefe0
cbs <chandra_mohan@intuit.com>
2020-12-01 10:40:35 -0800

    Changing project name from "SpringBootConsole" to "LearnSpringBoot"

---
+++ LearnSpringBoot/src/main/kotlin/com/bschandramohan/learn/springconnect/springbootconsole/service/CourseService.kt
@@ -0,0 +1,9 @@
+package com.bschandramohan.learn.springconnect.springbootconsole.service
+
+import com.bschandramohan.learn.springconnect.springbootconsole.entity.Course
+
+interface CourseService {
+    fun get(id: Long): Course
+    fun list(): List<Course>
+    fun save(course: Course): Course
+}
---
+++ LearnSpringBoot/src/test/kotlin/com/bschandramohan/learn/springconnect/springbootconsole/SpringBootConsoleApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.bschandramohan.learn.springconnect.springbootconsole
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootConsoleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
