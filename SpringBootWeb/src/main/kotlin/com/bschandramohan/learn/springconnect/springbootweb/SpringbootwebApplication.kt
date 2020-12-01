eecf8684bf7f902dc8e145953921bf4d4afcfb16
cbs <chandra_mohan@intuit.com>
2020-12-01 13:49:53 -0800

    Initial version of spring boot web app. Uses start.spring.io from IntelliJ Idea new project dialog. The only other change is for tomcat to start at port 8081 and application.properties to application.yaml

---
+++ SpringBootWeb/src/main/kotlin/com/bschandramohan/learn/springconnect/springbootweb/SpringbootwebApplication.kt
@@ -0,0 +1,11 @@
+package com.bschandramohan.learn.springconnect.springbootweb
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootwebApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringbootwebApplication>(*args)
+}
