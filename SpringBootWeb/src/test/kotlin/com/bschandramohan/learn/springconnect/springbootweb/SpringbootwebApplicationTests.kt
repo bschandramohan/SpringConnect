385f36e19565f43f1b1ced0f550514a547395238
bschandramohan <bschandramohan@gmail.com>
2020-12-01 13:49:53 -0800

    Initial version of spring boot web app. Uses start.spring.io from IntelliJ Idea new project dialog. The only other change is for tomcat to start at port 8081 and application.properties to application.yaml

---
+++ SpringBootWeb/src/test/kotlin/com/bschandramohan/learn/springconnect/springbootweb/SpringbootwebApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bschandramohan.learn.springconnect.springbootweb
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringbootwebApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
