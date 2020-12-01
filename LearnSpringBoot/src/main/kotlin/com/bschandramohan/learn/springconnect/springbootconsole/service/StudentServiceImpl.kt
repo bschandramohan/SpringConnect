3524f39a238fb58df0a24def5ef05938260cfcba
bschandramohan <bschandramohan@gmail.com>
2020-12-01 10:40:35 -0800

    Changing project name from "SpringBootConsole" to "LearnSpringBoot"

---
+++ LearnSpringBoot/src/main/kotlin/com/bschandramohan/learn/springconnect/springbootconsole/service/StudentServiceImpl.kt
@@ -0,0 +1,56 @@
+package com.bschandramohan.learn.springconnect.springbootconsole.service
+
+import com.bschandramohan.learn.springconnect.springbootconsole.entity.Course
+import com.bschandramohan.learn.springconnect.springbootconsole.entity.Student
+import com.bschandramohan.learn.springconnect.springbootconsole.repository.StudentRepository
+import org.springframework.stereotype.Service
+
+@Service
+class StudentServiceImpl(var studentRepository: StudentRepository, var courseService: CourseService) : StudentService {
+    override fun get(id: Long): Student {
+        val student = studentRepository.get(id)
+
+        val courseList = courseService.list()
+        mapCourses(student, courseList)
+
+        return student
+    }
+
+    private fun mapCourses(student: Student, courseList: List<Course>) {
+        student.courses.map {
+            if (it.name.isEmpty()) {
+                it.name = courseList.first { course -> course.id == it.id }.name
+            }
+        }
+    }
+
+    override fun list(): List<Student> {
+        val studentList = studentRepository.list()
+        val courseList = courseService.list()
+
+        studentList.forEach { student ->
+            mapCourses(student, courseList)
+        }
+
+        return studentList
+    }
+
+    override fun save(student: Student): Student {
+        studentRepository.save(student)
+        return studentRepository.get(student.id)
+    }
+
+    override fun addCourse(id: Long, courseId: Long) {
+        val student = studentRepository.get(id)
+
+        // Check if course is already added
+        val courseAlreadyAdded = student.courses.firstOrNull { it.id == courseId }
+        if (courseAlreadyAdded == null) {
+            // If not already added, then add it after fetching the course name
+            val courseList = courseService.list()
+            val correspondingCourse = courseList.firstOrNull { it.id == courseId }
+                    ?: throw Exception("Course ID passed is invalid")
+            student.courses.add(correspondingCourse)
+        }
+    }
+}
---
+++ LearnSpringBoot/src/test/kotlin/com/bschandramohan/learn/springconnect/springbootconsole/UnitTests.kt
@@ -0,0 +1,54 @@
+package com.bschandramohan.learn.springconnect.springbootconsole
+
+import com.bschandramohan.learn.springconnect.springbootconsole.entity.Course
+import com.bschandramohan.learn.springconnect.springbootconsole.entity.Student
+import com.bschandramohan.learn.springconnect.springbootconsole.repository.StudentRepository
+import com.bschandramohan.learn.springconnect.springbootconsole.service.CourseService
+import com.bschandramohan.learn.springconnect.springbootconsole.service.StudentServiceImpl
+import org.junit.jupiter.api.Test
+import org.mockito.ArgumentMatchers.anyLong
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UnitTests {
+    @Mock
+    private lateinit var courseService: CourseService
+
+    @Mock
+    private lateinit var studentRepository: StudentRepository
+
+    @InjectMocks
+    private lateinit var studentService: StudentServiceImpl
+
+    val student1ToSave = Student(1L, "Chandra")
+    val student2ToSave = Student(2L, "Mohan")
+    val courseToAdd = Course(1000L, "ML Course")
+
+    @Test
+    fun testGetStudentWithoutCourses() {
+        Mockito.`when`(studentRepository.get(anyLong())).thenReturn(student1ToSave)
+
+        studentService.save(student1ToSave)
+        val result = studentService.get(1L)
+
+        assert(result.name == "Chandra")
+        assert(result.courses.size == 0)
+    }
+
+    @Test
+    fun testGetStudentWithCourses() {
+        Mockito.`when`(studentRepository.get(anyLong())).thenReturn(
+                student2ToSave.apply { courses = mutableListOf(courseToAdd) })
+        Mockito.`when`(courseService.list()).thenReturn(listOf(courseToAdd))
+
+        studentService.save(student2ToSave)
+        studentService.addCourse(2L, 1000L)
+
+        val result = studentService.get(2L)
+        assert(result.courses.size == 1)
+        assert(result.courses[0].name == courseToAdd.name)
+    }
+}
