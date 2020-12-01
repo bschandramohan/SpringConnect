eab72d3dfee89f388d737c4b494a774e2d3d3199
cbs <chandra_mohan@intuit.com>
2020-11-30 20:48:50 -0800

    Fixed an issue where course was getting added even if it was already added for the student

--- SpringBootConsole/src/main/kotlin/com/bschandramohan/learn/springconnect/springbootconsole/service/StudentServiceImpl.kt
+++ SpringBootConsole/src/main/kotlin/com/bschandramohan/learn/springconnect/springbootconsole/service/StudentServiceImpl.kt
@@ -43,8 +43,14 @@ class StudentServiceImpl(var studentRepository: StudentRepository, var courseSer
     override fun addCourse(id: Long, courseId: Long) {
         val student = studentRepository.get(id)
 
-        val courseList = courseService.list()
-        val correspondingCourse = courseList.first { it.id == courseId }
-        student.courses.add(correspondingCourse)
+        // Check if course is already added
+        val courseAlreadyAdded = student.courses.firstOrNull { it.id == courseId }
+        if (courseAlreadyAdded == null) {
+            // If not already added, then add it after fetching the course name
+            val courseList = courseService.list()
+            val correspondingCourse = courseList.firstOrNull { it.id == courseId }
+                ?: throw Exception("Course ID passed is invalid")
+            student.courses.add(correspondingCourse)
+        }
     }
 }
