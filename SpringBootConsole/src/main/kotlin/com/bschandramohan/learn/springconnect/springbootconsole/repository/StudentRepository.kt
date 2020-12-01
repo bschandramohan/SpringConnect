4dbc74566faa11d12d67bfcf52b80beeb5ba1ec5
bschandramohan <bschandramohan@gmail.com>
2020-12-01 10:39:18 -0800

    Changing project name from "SpringBootConsole" to "LearnSpringBoot"

--- SpringBootConsole/src/main/kotlin/com/bschandramohan/learn/springconnect/springbootconsole/repository/StudentRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.bschandramohan.learn.springconnect.springbootconsole.repository
-
-import com.bschandramohan.learn.springconnect.springbootconsole.entity.Student
-
-interface StudentRepository {
-    fun get(id: Long) : Student
-    fun list() : List<Student>
-    fun save(student: Student) : Student
-}
