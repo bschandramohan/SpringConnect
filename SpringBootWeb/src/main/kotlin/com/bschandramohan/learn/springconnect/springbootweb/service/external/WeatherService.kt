a9a4006d07ab803db4a6ed3bcdce4cc3876a02ab
bschandramohan <bschandramohan@gmail.com>
2020-12-02 20:35:14 -0800

    Fixing a build issue

--- SpringBootWeb/src/main/kotlin/com/bschandramohan/learn/springconnect/springbootweb/service/external/WeatherService.kt
+++ SpringBootWeb/src/main/kotlin/com/bschandramohan/learn/springconnect/springbootweb/service/external/WeatherService.kt
@@ -1,5 +1,5 @@
 package com.bschandramohan.learn.springconnect.springbootweb.service.external
 
 interface WeatherService {
-    fun getWeather(zipCode: String = "95014")
+    fun getWeather(zipCode: String = "95014"): String
 }
