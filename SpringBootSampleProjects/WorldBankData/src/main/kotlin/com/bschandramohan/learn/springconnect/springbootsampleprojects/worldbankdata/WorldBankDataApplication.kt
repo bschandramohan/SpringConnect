d68b5531706a7904896c742d7ba0330e1190c653
bschandramohan <bschandramohan@gmail.com>
2020-12-27 02:56:05 -0800

    Renaming application class

--- SpringBootSampleProjects/WorldBankData/src/main/kotlin/com/bschandramohan/learn/springconnect/springbootsampleprojects/worldbankdata/WorldBankDataApplication.kt
+++ SpringBootSampleProjects/WorldBankData/src/main/kotlin/com/bschandramohan/learn/springconnect/springbootsampleprojects/worldbankdata/WorldBankDataApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class WorldbankdataApplication
+class WorldBankDataApplication
 
 fun main(args: Array<String>) {
-    runApplication<WorldbankdataApplication>(*args)
+    runApplication<WorldBankDataApplication>(*args)
 }
