6ecc2123f9a1f5d5773eb0f74f184a4b1a6e3a57
bschandramohan <bschandramohan@gmail.com>
2020-12-27 01:20:24 -0800

    Based off Spring 5.0 Projects by Nilang Patel <Packt>, spring boot rest application that displays country GDP by querying Rest endpoint of WorldBank (e.g., http://api.worldbank.org/v2/countries/IN/indicators/NY.GDP.MKTP.CD?format=json&date=2019).

---
+++ SpringBootSampleProjects/WorldBankData/src/main/kotlin/com/bschandramohan/learn/springconnect/springbootsampleprojects/worldbankdata/WorldbankdataApplication.kt
@@ -0,0 +1,11 @@
+package com.bschandramohan.learn.springconnect.springbootsampleprojects.worldbankdata
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WorldbankdataApplication
+
+fun main(args: Array<String>) {
+    runApplication<WorldbankdataApplication>(*args)
+}
---
+++ SpringBootSampleProjects/WorldBankData/src/test/kotlin/com/bschandramohan/learn/springconnect/springbootsampleprojects/worldbankdata/WorldbankdataApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bschandramohan.learn.springconnect.springbootsampleprojects.worldbankdata
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WorldbankdataApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
