a1f7f101b4aa529eecffadac8ede2211541660f3
bschandramohan <bschandramohan@gmail.com>
2020-12-27 21:12:11 -0800

    Initial commit for Share Links Service/App

---
+++ SpringBootSampleProjects/ShareLinks/src/main/kotlin/com/bschandramohan/app/sharelinks/ShareLinksApplication.kt
@@ -0,0 +1,11 @@
+package com.bschandramohan.app.sharelinks
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ShareLinksApplication
+
+fun main(args: Array<String>) {
+    runApplication<ShareLinksApplication>(*args)
+}
---
+++ SpringBootSampleProjects/ShareLinks/src/test/kotlin/com/bschandramohan/app/sharelinks/ShareLinksApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bschandramohan.app.sharelinks
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ShareLinksApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
