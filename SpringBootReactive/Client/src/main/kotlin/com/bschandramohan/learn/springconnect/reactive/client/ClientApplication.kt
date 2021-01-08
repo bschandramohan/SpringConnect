fdfa3dc746d5c75205d95c18a25fd47073576590
bschandramohan <bschandramohan@gmail.com>
2021-01-07 22:50:57 -0800

    Client Application changes to test the Annotation based and  router based spring reactive web service

--- SpringBootReactive/Client/src/main/kotlin/com/bschandramohan/learn/springconnect/reactive/client/ClientApplication.kt
+++ SpringBootReactive/Client/src/main/kotlin/com/bschandramohan/learn/springconnect/reactive/client/ClientApplication.kt
@@ -1,11 +1,19 @@
 package com.bschandramohan.learn.springconnect.reactive.client
 
-import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
+import org.springframework.web.reactive.function.client.WebClient
 
-@SpringBootApplication
-class ClientApplication
+class ClientApplication {
+    private val client = WebClient.create("http://localhost:8080")
+
+    fun getResult(uriParam: String) = client.get()
+        .uri(uriParam)
+        .retrieve().bodyToMono(String::class.java).block()
+}
 
 fun main(args: Array<String>) {
-    runApplication<ClientApplication>(*args)
+    println("Controller (Annotation based) Vowels Result=${ClientApplication().getResult("/controller/vowels")}")
+    println("Controller (Annotation based) Consonants Result=${ClientApplication().getResult("/controller/consonants")}")
+
+    println("Router Vowels Result=${ClientApplication().getResult("/router/vowels")}")
+    println("Router Consonants Result=${ClientApplication().getResult("/router/consonants")}")
 }
