13545f23e14f899c2537942538b042d700cc5bab
bschandramohan <bschandramohan@gmail.com>
2021-01-07 22:33:25 -0800

    Easiest way of getting rest endpoints using Spring webflux - using Annotations.
    Reference: https://spring.io/blog/2019/04/12/going-reactive-with-spring-coroutines-and-kotlin-flow

---
+++ SpringBootReactive/Service/src/main/kotlin/com/bschandramohan/learn/springconnect/reactive/service/controller/LettersReactiveController.kt
@@ -0,0 +1,23 @@
+package com.bschandramohan.learn.springconnect.reactive.service.controller
+
+import com.bschandramohan.learn.springconnect.reactive.service.repository.consonants
+import com.bschandramohan.learn.springconnect.reactive.service.repository.vowels
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Flux
+import reactor.kotlin.core.publisher.toFlux
+
+@RestController
+@RequestMapping("controller")
+class LettersReactiveController {
+    @GetMapping("/vowels")
+    fun getVowels() : Flux<Char> {
+        return vowels().toFlux()
+    }
+
+    @GetMapping("/consonants")
+    fun getConsonants(): Flux<Char> {
+        return consonants().toFlux()
+    }
+}
