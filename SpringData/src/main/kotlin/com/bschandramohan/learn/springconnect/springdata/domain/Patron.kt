0d9337ab0214a15b2268b77761e4aeae95d94e9c
bschandramohan <bschandramohan@gmail.com>
2021-01-09 14:38:41 -0800

    updated comment

--- SpringData/src/main/kotlin/com/bschandramohan/learn/springconnect/springdata/domain/Patron.kt
+++ SpringData/src/main/kotlin/com/bschandramohan/learn/springconnect/springdata/domain/Patron.kt
@@ -7,7 +7,7 @@ import javax.persistence.Id
 import javax.persistence.Table
 
 /**
- * User who supports humanity by sharing useful/entertaining links
+ * User who supports humanity by sharing useful/entertaining links. Stored in PostGres database
  *
  * Can't there be namespaces? Tried to create the table as User and that's not allowed in PostgreSQL
  * https://stackoverflow.com/a/49573003/207552
