f9875bd0c74c00546086ec31593ea1dda089c673
bschandramohan <bschandramohan@gmail.com>
2021-02-07 16:05:45 -0800

    Totally optional - use tomcat instead of netty

--- SpringReactiveRedis/build.gradle.kts
+++ SpringReactiveRedis/build.gradle.kts
@@ -18,7 +18,10 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-data-redis-reactive")
-    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.springframework.boot:spring-boot-starter-webflux") {
+        exclude("org.springframework.boot:spring-boot-starter-netty")
+    }
+    implementation("org.springframework.boot:spring-boot-starter-tomcat")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
